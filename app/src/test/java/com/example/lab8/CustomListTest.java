146b59ccb1802d1f3c59c9cf6739e1c64a3f6a3d
NavrasK <trailmixerstudios@gmail.com>
2023-03-08 17:41:52 -0700

    Broken test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -7,10 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
+
+
+
 public class CustomListTest {
+    // Add tests here
+    private CustomList list;
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
-
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize+1);
+    }
 }
